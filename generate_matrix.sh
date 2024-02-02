56ebaf2ac9af626664926f2451797177c0733e77
Theo Bob Massard <tbobm@protonmail.com>
2024-02-02 16:42:53 +0100

    docs(matrix): document generation script
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- generate_matrix.sh
+++ generate_matrix.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+# Generate a dynamic matrix input by retrieving every app directory containing
+# a Dockerfile
+# outputs the following:
+#
+# target:
+#   - src: ./sqs/consumer
+#     name: ./sqs/consumer/Dockerfile
+#   - src: ./sqs/producer
+#     name: ./sqs/producer/Dockerfile
 targets=$(find -name Dockerfile)
 
 echo "target:"
