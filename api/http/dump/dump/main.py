727496d2b07d3b7f6f1417ba09fbe15a81160544
Theo Bob Massard <tbobm@protonmail.com>
2024-04-19 12:48:55 +0200

    feat(http-dump): dump json, form and headers
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- api/http/dump/dump/main.py
+++ api/http/dump/dump/main.py
@@ -1,15 +1,21 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.api_route("/{full_path:path}", methods=["HEAD", "GET", "POST", "DELETE", "PUT"])
+async def read_root(request: Request, full_path: str):
+    response = {
+        "path": full_path,
+        "method": request.method,
+    }
+    response['headers'] = request.headers
+    _json = await request.json()
+    if _json is not None:
+        response['json'] = _json
+    _form = await request.form()
+    if _form is not None:
+        response['form'] = _form
+    return response
